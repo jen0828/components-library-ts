c1229a7e352f8ab02c56dc3ea1909ff8b6ca83e6
Jenny Moon <jenny0moon@gmail.com>
2022-05-03 20:29:11 +0100

    Add styles

--- src/Button.tsx
+++ src/Button.tsx
@@ -8,7 +8,11 @@ export interface Props extends HTMLAttributes<HTMLButtonElement>{
 export const Button = ({children, variant, ...props}: Props) => {
   return (
     <button {...props} style={{backgroundColor: variant === 'primary' ? 'blue' : 'grey',
-    color: 'white'
+    color: 'white',
+    border: 'none',
+    borderRadius: 100,
+    padding: 10,
+    cursor: 'pointer'
     }}>
      {children}
     </button>
